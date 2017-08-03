0af48a08e3d32877a5942585a45586822a981aa4
Juan David <cantejuandavid@gmail.com>
2017-08-03 17:50:28 -0500

    styling

--- src/components/artist.vue
+++ src/components/artist.vue
@@ -1,16 +1,18 @@
 <template lang="pug">
     li.artist
         h2: a(:href="artist.url" target="_blank") {{artist.name}}
-        img(:src="artist.image[2]['#text']")
+        img(:src="artist.image[2]['#text']")    
 </template>
+
 <script>
 export default {
   name: 'Artist',
   props: ['artist']
 }
 </script>
+
 <style lang="stylus" scoped>
 li.artist
-	display block
-	margin 10px auto
+	display inline-block
+	margin 10px 10px
 </style>
